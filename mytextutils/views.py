9b32c0065cbb78485b5d5e465ce75d043ab9600b
nagvenkat.bypilla@gmail.com <nagvenkat.bypilla@gmail.com>
2020-09-04 19:52:53 +0530

    Added New features

--- mytextutils/views.py
+++ mytextutils/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import string
+import datetime
 
 # Create your views here.
 def home(request):
@@ -8,12 +9,15 @@ def home(request):
 
 def analyzetext(request):
     text=request.GET.get('textbox','default')
+    charcount = len(text) 
     ctext=request.GET.get('removepunc','Off')
     caps=request.GET.get('firstcaps','Off')
+    spaceremove=request.GET.get('spaceremover','Off')
+    newlineremover=request.GET.get('newlineremover','Off')
     
     #print(ctext)
     if ctext=='on':
-            
+           
         a=string.punctuation
         analyzetext = ''
         for text in text:
@@ -21,29 +25,55 @@ def analyzetext(request):
                 analyzetext += text
         params = {
             'purpose' : "Analysed Text : ",
-            "b": analyzetext
+            "b": analyzetext,
+            'count' : charcount
         }
         return render(request , 'analyze.html', params)
+        
     elif(caps=='on'):
         analyzetext=''
         for char in text:
             analyzetext = analyzetext + char.upper()
         params = {
             'purpose' : 'Analysed Text :',
-            'b' : analyzetext
+            'b' : analyzetext,
+            'count' : charcount
         }
         return render(request,'analyze.html',params)
 
+    elif(spaceremove=='on'):
+        analyzetext = ""
+        for i in range(len(text)):
+            if text[i] == " ":
+                if text[i+1] == " ":
+                    analyzetext += text[i]
+            
+            elif(text[i]!=" "):
+                analyzetext += text[i]
+        
+        params = {
+            'purpose' : 'Analysed Text :',
+            'b' : analyzetext,
+            'count' : charcount
+        }
+              
+        return render(request,'analyze.html',params)
+
+    elif(newlineremover=='on'):
+        analyzetext=''
+        for char in text:
+            if char != "\n":
+                analyzetext += char
+        params = {
+            'purpose' : 'Newlines Removed',
+            'b' : analyzetext,
+            'count' : charcount
+        }
+        return render(request,'analyze.html', params)
+
+
+
     else:
-        return HttpResponse("Error")
+        return HttpResponse("Please Select Any Option")
 
 
-
-def newlineremover(request):
-    return HttpResponse("newlineremover")
-
-def spaceremover(request):
-    return HttpResponse("spaceremover")
-
-def charcount(request):
-    return HttpResponse("charcount")
